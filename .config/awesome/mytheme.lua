3d80d042340cdaa274eaab4b80f07a9fb269fea0
Josh Paterson <mail@joshpaterson.com>
2021-09-20 23:44:12 -0500

    Add srcery theme to awesome

--- .config/awesome/mytheme.lua
+++ .config/awesome/mytheme.lua
@@ -7,24 +7,61 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
+-- Srcery colors.
+local srcery_colors = {}
+srcery_colors.black = "#1C1B19"
+srcery_colors.red = "#EF2F27"
+srcery_colors.green = "#519F50"
+srcery_colors.yellow = "#FBB829"
+srcery_colors.blue = "#2C78BF"
+srcery_colors.magenta = "#E02C6D"
+srcery_colors.cyan = "#0AAEB3"
+srcery_colors.white = "#D0BFA1"
+srcery_colors.brightblack = "#918175"
+srcery_colors.brightred = "#F75341"
+srcery_colors.brightgreen = "#98BC37"
+srcery_colors.brightyellow = "#FED06E"
+srcery_colors.brightblue = "#68A8E4"
+srcery_colors.brightmagenta = "#FF5C8F"
+srcery_colors.brightcyan = "#53FDE9"
+srcery_colors.brightwhite = "#FCE8C3"
+
+-- Theme setting.
+theme.bg_normal = srcery_colors.black
+theme.bg_focus = srcery_colors.blue
+theme.bg_urgent = srcery_colors.brightred
+theme.bg_minimize = srcery_colors.brightblack
+theme.bg_systray = theme.bg_normal
+
+theme.fg_normal = srcery_colors.white
+theme.fg_focus = srcery_colors.brightwhite
+theme.fg_urgent = srcery_colors.brightwhite
+theme.fg_minimize = srcery_colors.brightwhite
+
+theme.useless_gap = dpi(0)
+theme.border_width = dpi(1)
+theme.border_normal = srcery_colors.black
+theme.border_focus = srcery_colors.blue
+theme.border_marked = srcery_colors.magenta
+
 theme.font          = "sans 8"
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+-- theme.bg_normal     = "#222222"
+-- theme.bg_focus      = "#535d6c"
+-- theme.bg_urgent     = "#ff0000"
+-- theme.bg_minimize   = "#444444"
+-- theme.bg_systray    = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+-- theme.fg_normal     = "#aaaaaa"
+-- theme.fg_focus      = "#ffffff"
+-- theme.fg_urgent     = "#ffffff"
+-- theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
-theme.border_width  = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+-- theme.useless_gap   = dpi(0)
+-- theme.border_width  = dpi(1)
+-- theme.border_normal = "#000000"
+-- theme.border_focus  = "#535d6c"
+-- theme.border_marked = "#91231c"
 
 -- There are other variable sets
 -- overriding the default one when
