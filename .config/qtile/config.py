31e41d36862544866bdd557d0f5d957ab5dd5383
Josh Paterson <mail@joshpaterson.com>
2021-10-02 19:55:50 -0500

    Make font bigger, add program shortcuts

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -4,61 +4,67 @@ from libqtile import bar, layout, widget, hook
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 
-mod = "mod4"
-terminal = "kitty"
+mod = 'mod4'
+terminal = 'kitty'
 
 keys = [
     # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    Key([mod], 'h', lazy.layout.left(), desc='Move focus to left'),
+    Key([mod], 'l', lazy.layout.right(), desc='Move focus to right'),
+    Key([mod], 'j', lazy.layout.down(), desc='Move focus down'),
+    Key([mod], 'k', lazy.layout.up(), desc='Move focus up'),
+    Key([mod], 'space', lazy.layout.next(), desc='Move window focus to other window'),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, 'shift'], 'h', lazy.layout.shuffle_left(), desc='Move window to the left'),
+    Key([mod, 'shift'], 'l', lazy.layout.shuffle_right(), desc='Move window to the right'),
+    Key([mod, 'shift'], 'j', lazy.layout.shuffle_down(), desc='Move window down'),
+    Key([mod, 'shift'], 'k', lazy.layout.shuffle_up(), desc='Move window up'),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, 'control'], 'h', lazy.layout.grow_left(), desc='Grow window to the left'),
+    Key([mod, 'control'], 'l', lazy.layout.grow_right(), desc='Grow window to the right'),
+    Key([mod, 'control'], 'j', lazy.layout.grow_down(), desc='Grow window down'),
+    Key([mod, 'control'], 'k', lazy.layout.grow_up(), desc='Grow window up'),
+    Key([mod], 'n', lazy.layout.normalize(), desc='Reset all window sizes'),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod, 'shift'], 'Return', lazy.layout.toggle_split(), 
+        desc='Toggle between split and unsplit sides of stack'),
 
     # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], 'Tab', lazy.next_layout(), desc='Toggle between layouts'),
+    Key([mod], 'w', lazy.window.kill(), desc='Kill focused window'),
 
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # Start & Shutdown qtile
+    Key([mod, 'control'], 'r', lazy.restart(), desc='Restart Qtile'),
+    Key([mod, 'control'], 'q', lazy.shutdown(), desc='Shutdown Qtile'),
+    
+    # Program launch bindings
+    Key([mod], 'r', lazy.spawn('rofi -show run'), desc='Launch Rofi'),
+    Key([mod], 'Return', lazy.spawn(terminal), desc='Launch terminal'),
+    Key([mod], 'f', lazy.spawn(terminal + ' ranger'), desc='Launch Ranger'),
 ]
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in '123456789']
 
 for i in groups:
     keys.extend([
         # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
+        Key([mod], i.name, lazy.group[i.name].toscreen(), desc='Switch to group {}'.format(i.name)),
 
         # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True), desc="Switch to & move focused window to group {}".format(i.name)),
+        Key([mod, 'shift'], i.name, lazy.window.togroup(i.name, switch_group=True), 
+            desc='Switch to & move focused window to group {}'.format(i.name)),
         # Or, use below if you prefer not to switch to that group.
         # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+        # Key([mod, 'shift'], i.name, lazy.window.togroup(i.name),
+        #     desc='move focused window to group {}'.format(i.name)),
     ])
 
 layouts = [
@@ -78,7 +84,7 @@ layouts = [
 
 widget_defaults = dict(
     font='sans',
-    fontsize=12,
+    fontsize=24,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
@@ -89,30 +95,28 @@ screens = [
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
+#                widget.Prompt(),
+#                widget.WindowName(),
                 widget.Chord(
                     chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
+                        'launch': ('#ff0000', '#ffffff'),
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 widget.Systray(),
                 widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
                 widget.Volume(),
             ],
-            24,
+            48,
         ),
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag([mod], 'Button1', lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], 'Button3', lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Click([mod], 'Button2', lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
@@ -131,7 +135,7 @@ floating_layout = layout.Floating(float_rules=[
     Match(title='pinentry'),  # GPG key password entry
 ])
 auto_fullscreen = True
-focus_on_window_activation = "smart"
+focus_on_window_activation = 'smart'
 reconfigure_screens = True
 
 # If things like steam games want to auto-minimize themselves when losing
@@ -142,7 +146,7 @@ auto_minimize = True
 def start_once():
     subprocess.call(['nitrogen', '--restore'])
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
 # mailing lists, GitHub issues, and other WM documentation that suggest setting
 # this string if your java app doesn't work correctly. We may as well just lie
@@ -150,4 +154,4 @@ def start_once():
 #
 # We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
 # java that happens to be on java's whitelist.
-wmname = "LG3D"
+wmname = 'LG3D'
